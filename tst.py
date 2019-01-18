6ff4448404f7962ddc43338fca78d70c66e2ade7
alireza parchami <alirezaprm76@gmail.com>
2019-01-18 14:43:39 +0330

    implement Holdout function

--- tst.py
+++ tst.py
@@ -1,44 +1,48 @@
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
-import numpy as np
+from random import sample
+import math
 
 
 def hold_out(data):
-    print(data)
-    X = [[0, 0], [1, 1], [2, 2], [3, 3]]
+    test_set_size = math.floor(len(data.index) / 3)
+    test_set_indexes = sample(range(0,len(data.index)), test_set_size)
+    test_set = pd.DataFrame(data, index=test_set_indexes)
+    train_set = data.drop(test_set_indexes)
+    return train_set, test_set
+
+
+def my_test():
+    df = pd.DataFrame(
+        {'col1': [1, 2, 3, 4, 5, 6, 7], 'col2': list('ebd?aba'), 'col3': list('?bcdbbr'), 'col4': list('eewcae?')})
+    print(df)
+    test_set_size = math.floor(len(df.index) / 3)
+    test_set_indexes = sample(range(0, len(df.index)), test_set_size)
+    test_set = pd.DataFrame(df, index=test_set_indexes)
+    print("test_set_size: ", test_set_size)
+    print("test_set_indexes: ", test_set_indexes)
+    print("## Test Set \n", test_set, "\n##")
+    train_set = df.drop(test_set_indexes)
 
 
 def test():
-    df = pd.DataFrame(
-        {'col1': [1, 2, 3, 4, 5, 6], 'col2': list('ebd?ab'), 'col3': list('?bcdbb'), 'col4': list('eecae?')})
-    mod = df['col4'].mode()[0]
-    print("mod: ", mod , "  type: " , type(mod))
-    print(df)
-    print("----------------")
-    for col in df.columns:
-        mod = df[col].mode()[0]
-        df[col] = df[col].replace('?', mod)
-        #for col in df.columns:
-        #    a = col.strip().replace("a" , mod)
-    print(df)
+    testing_set_data = (pd.DataFrame(data, columns=range(1, 22))).values.tolist()
+    testing_set_result = (pd.DataFrame(data, columns=[0])).values.tolist()
 
 
 def prog(data):
-    training_set = (pd.DataFrame(data, columns=range(1, 22))).values.tolist()
+    training_set_data = (pd.DataFrame(data, columns=range(1, 22))).values.tolist()
     training_set_result = (pd.DataFrame(data, columns=[0])).values.tolist()
     # training_set_result reformat
     tsr_reformat = []
     for i in range(0, len(training_set_result)):
         tsr_reformat.append(training_set_result[i][0])
     print(tsr_reformat)
-    print(training_set)
+    print(training_set_data)
     print(training_set_result)
     neigh = KNeighborsClassifier(n_neighbors=3)
-    neigh.fit(training_set, tsr_reformat)
-#  print
-    sample = training_set[0:2]
-    print("sample: ", sample)
-    print("sample predict: ", neigh.predict(sample))
+    neigh.fit(training_set_data, tsr_reformat)
+    return neigh
 
 
 # fill missing value with mode
@@ -46,6 +50,7 @@ def fill_missing(data):
     for col in data.columns:
         mod = data[col].mode()[0]
         data[col] = data[col].replace('?', mod)
+
 
 # convert nominal to numeric data
 def nominal_to_numeric(data):
@@ -58,7 +63,9 @@ def nominal_to_numeric(data):
 data = pd.read_csv('data/agaricus-lepiota.data', sep=",", header=None)
 fill_missing(data)
 nominal_to_numeric(data)
-prog(data)
-# test()
+training_set, testing_set = hold_out(data)
+knn = prog(training_set)
+#test()
+#my_test()
 # hold_out(data)
 # print(neigh.predict_proba([[0,1]]))
